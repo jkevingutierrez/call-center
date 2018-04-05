4d18fdd63ebc3edf39fee99ef576b1ed4f5ffacc
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2018-04-05 07:36:16 -0500

    Add some imports into test

--- src/test/java/com/almundo/test/callcenter/services/DispatcherServiceTests.java
+++ src/test/java/com/almundo/test/callcenter/services/DispatcherServiceTests.java
@@ -4,23 +4,35 @@ import com.almundo.test.callcenter.entities.Call;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DispatcherServiceTests {
 
-    @Autowired
+    @MockBean
     private DispatcherService dispatcherService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void dispatchOneNewCall() {
@@ -34,6 +46,12 @@ public class DispatcherServiceTests {
         // int numberOfCalls = 10;
         // List<Call> calls = dispatcherService.dispatchNewCalls(numberOfCalls);
         // assertEquals(calls.size(), numberOfCalls);
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        // this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        //         .andExpect(content().string(containsString("Hello World")));
     }
 
 }
