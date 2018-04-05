69dd11f1f90f5d315b0372bd4dcfe3a925eb4366
Kevin Gutierrez <kevin.gutierrezg@rappi.com>
2018-04-05 16:55:11 -0500

    Add FixMethodOrder to DispatcherServiceTests

--- src/test/java/com/almundo/test/callcenter/services/DispatcherServiceTests.java
+++ src/test/java/com/almundo/test/callcenter/services/DispatcherServiceTests.java
@@ -4,8 +4,10 @@ import com.almundo.test.callcenter.entities.Call;
 import com.almundo.test.callcenter.entities.Employee;
 import com.almundo.test.callcenter.repositories.EmployeeRepository;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -24,6 +26,7 @@ import static org.springframework.boot.jdbc.EmbeddedDatabaseConnection.H2;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE, connection = H2)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DispatcherServiceTests {
 
     @Autowired
@@ -38,6 +41,7 @@ public class DispatcherServiceTests {
     @Before
     public void setUp() {
         int numberOfEmployees = 10;
+
         for (int i = 1; i <= numberOfEmployees; i++) {
             Employee employee = new Employee("Operator " + i, Employee.Type.OPERATOR);
             employeeRepository.save(employee);
@@ -46,20 +50,20 @@ public class DispatcherServiceTests {
 
 
     @Test
-    public void should_dispatch_one_new_call() throws InterruptedException {
+    public void a_should_dispatch_one_new_call() throws InterruptedException {
         Call call = service.dispatchNewCall();
 
-        TimeUnit.SECONDS.sleep(maxDuration + 1);
+        TimeUnit.SECONDS.sleep(maxDuration);
 
         validateFinishedCall(call);
     }
 
     @Test
-    public void should_dispatch_ten_calls_in_parallel() throws InterruptedException {
+    public void b_should_dispatch_ten_calls_in_parallel() throws InterruptedException {
         int numberOfCalls = 10;
         List<Call> calls = service.dispatchNewCalls(numberOfCalls);
 
-        TimeUnit.SECONDS.sleep(maxDuration + 1);
+        TimeUnit.SECONDS.sleep(maxDuration);
 
         assertThat(calls).hasSize(numberOfCalls);
         for (Call call : calls) {
@@ -68,9 +72,9 @@ public class DispatcherServiceTests {
     }
 
     @Test(expected = AssertionError.class)
-    public void should_take_more_than_max_duration_to_dispatch_more_than_ten_calls_in_parallel() throws InterruptedException {
+    public void c_should_take_more_than_max_duration_to_dispatch_more_than_ten_calls_in_parallel() throws InterruptedException {
 
-        int numberOfCalls = 11;
+        int numberOfCalls = 21;
         List<Call> calls = service.dispatchNewCalls(numberOfCalls);
 
         TimeUnit.SECONDS.sleep(maxDuration);
